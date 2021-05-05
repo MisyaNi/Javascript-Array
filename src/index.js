2bc8d6b31a1a19131d5fa34cc98c077e6702f5d7
Misyani-DS <54836758+Misyani-DS@users.noreply.github.com>
2021-05-05 17:46:19 +0800

    8 js array

--- src/index.js
+++ src/index.js
@@ -16,3 +16,65 @@ const filterItems = items.filter((item) => {
 
 console.log(items)
 console.log(filterItems)
+
+//2. Map - to display certain key or once for each element in an array
+
+const itemNames = items.map((item) => {
+  return item.name
+})
+
+console.log(itemNames)
+
+const itemPrice = items.map((item) => {
+  return item.price
+})
+
+console.log(itemPrice)
+
+//3. find 
+
+const foundItem = items.find((item) => {
+  return item.name === 'Book'
+})
+
+console.log(foundItem)
+
+//4. forEach
+
+items.forEach((item) => {
+  console.log(item.name)
+})
+
+//5. some
+
+const hasInexpensiveItems = items.some((item) => {
+  return item.price <= 100
+})
+
+console.log(hasInexpensiveItems)
+
+//6. every
+
+const checkItems = items.every((item) => {
+  return item.price <= 1000
+})
+
+console.log(checkItems)
+
+//7. reduce
+
+const total = items.reduce((currentTotal, item) => {
+  return item.price + currentTotal
+}, 0)
+
+console.log(total)
+
+//8. includes
+
+const numbers = [1, 2, 5, 8]
+
+const includesNum1 = numbers.includes(6) //return false
+const includesNum2 = numbers.includes(8) //return true
+
+console.log(includesNum1)
+console.log(includesNum2)
